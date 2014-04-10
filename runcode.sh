4d8d45e5f243dad0fcefd1734e7c07d53e922eee
Kali Allison <kallison@stanford.edu>
2014-04-10 16:06:33 -0700

    added shell files to run code

---
+++ runcode.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -N eq_cycle_order2
+#PBS -l nodes=1:ppn=1
+#PBS -q tgp
+#PBS -V
+#PBS -m n
+#PBS -k oe
+#PBS -e eq_cycle_order2.err
+#PBS -o eq_cycle_order2.out
+#
+
+EXEC_DIR=/data/dunham/kallison/eqCycle
+cd $PBS_O_WORKDIR
+
+mpirun $EXEC_DIR/main -order 2 -Ny 151 -Nz 151
+#mpirun $EXEC_DIR/main -ksp_type gmres -ORDER 4 -Ny 151 -Nz 151
---
+++ submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf eq_cycle_order*
+
+printf "\nSubmitting job using: \n\n"
+cat runcode.sh
+printf "\n\n"
+
+qsub runcode.sh
+qstat | grep kallison | grep -v C
+RETVAL=$?
+while [ $RETVAL -eq 0 ]
+do
+  qstat | grep kallison | grep -v C
+  RETVAL=$?
+  sleep 5
+done
