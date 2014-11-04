919e1bbc61afd58e8ce7e17de500583e03d0ddbf
Kali Allison <kallison@stanford.edu>
2014-11-04 13:01:47 -0800

    added spmat files to git repository

---
+++ spmat.cpp
@@ -0,0 +1,271 @@
+#include "spmat.hpp"
+
+// constructor
+Spmat::Spmat(size_t rowSize, size_t colSize)
+:_rowSize(rowSize),_colSize(colSize)
+{}
+
+// copy constructor
+Spmat::Spmat(const Spmat& that)
+:_mat(that._mat),
+ _rowSize(that._rowSize),_colSize(that._colSize)
+{}
+
+void Spmat::eye()
+{
+  _mat.clear(); // ensure matrix is currently empty
+
+  size_t Ii;
+  for (Ii=0;Ii<_rowSize;Ii++) // iterate over rows
+  {
+    _mat[Ii][Ii] = 1.0;
+  }
+}
+
+
+void Spmat::print() const
+{
+  const_row_iter Ii;
+  const_col_iter Jj;
+  for(Ii=_mat.begin(); Ii!=_mat.end(); Ii++) // iterate over rows
+  {
+    for( Jj=(Ii->second).begin(); Jj!=(Ii->second).end(); Jj++)
+    {
+      std::cout << "(" << Ii->first << ",";
+      std::cout << Jj->first << "): ";
+      std::cout << Jj->second << std::endl;
+    }
+  }
+}
+
+// same as print, but formatted like PETSc
+void Spmat::printPetsc() const
+{
+  const_row_iter Ii;
+  const_col_iter Jj;
+  for(Ii=_mat.begin(); Ii!=_mat.end(); Ii++) // iterate over rows
+  {
+    std::cout << "row " << Ii->first << ": ";
+    for( Jj=(Ii->second).begin(); Jj!=(Ii->second).end(); Jj++)
+    {
+
+      std::cout << "(" << Jj->first << ", ";
+      std::cout << Jj->second << ") ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+
+// convert to PETSc style matrix
+// assumes matrix has already been allocated appropriately
+void Spmat::convert(Mat& petscMat) const
+{
+  PetscInt Istart,Iend;
+  MatGetOwnershipRange(petscMat,&Istart,&Iend);
+
+  PetscInt row,col;
+  double val = 0.0;
+
+  // iterate over nnz entries in this, placing them in PETSc matrix
+  const_row_iter Ii;
+  const_col_iter Jj;
+  for(Ii=_mat.begin(); Ii!=_mat.end(); Ii++) // iterate over rows
+  {
+    for( Jj=(Ii->second).begin(); Jj!=(Ii->second).end(); Jj++)
+    {
+      row = Ii->first;
+      col = Jj->first;
+      val = Jj->second;
+      if (row>=Istart && col<Iend) {
+        MatSetValues(petscMat,1,&row,1,&col,&val,INSERT_VALUES);
+      }
+    }
+  }
+  MatAssemblyBegin(petscMat,MAT_FINAL_ASSEMBLY);
+  MatAssemblyEnd(petscMat,MAT_FINAL_ASSEMBLY);
+}
+
+void Spmat::transpose()
+{
+  Spmat temp(size(1),size(2)); // copy input
+  row_iter Ii;
+  col_iter Jj;
+  for(Ii=_mat.begin(); Ii!=_mat.end(); Ii++) // iterate over rows
+  {
+    for( Jj=(Ii->second).begin(); Jj!=(Ii->second).end(); Jj++)
+    {
+      temp(Jj->first,Ii->first,Jj->second);
+    }
+  }
+  *this = temp;
+}
+
+// returns the size of the matrix along the specified direction
+// (1 = row, 2 = col, as in matlab)
+size_t Spmat::size(const int dim) const
+{
+  if (dim==1) { return _rowSize;}
+  else if (dim==2) { return _colSize;}
+  else {assert(0<1);} // I really need to learn how to throw exceptions
+  return 0;
+}
+
+void Spmat::scale(double val)
+{
+  row_iter Ii;
+  col_iter Jj;
+
+  for (Ii=_mat.begin();Ii!=_mat.end();Ii++) // iterate over rows
+  {
+    for(Jj=Ii->second.begin();Jj!=Ii->second.end();Jj++) // iterate over cols
+    {
+      Jj->second = Jj->second*val;
+    }
+  }
+}
+
+// preforms Kronecker product
+Spmat kron(const Spmat& left,const Spmat& right)
+{
+  size_t leftRowSize = left.size(1);
+  size_t leftColSize = left.size(2);
+  size_t rightRowSize = right.size(1);
+  size_t rightColSize = right.size(2);
+
+  Spmat result(leftRowSize*rightRowSize,leftColSize*rightColSize);
+
+
+  //~double val = 0.0;
+  //~size_t Ii,Jj;
+  //~// naive implementation loops over all possible entries ( very slow)
+  //~for (Ii=0;Ii<leftRowSize*rightRowSize;Ii++)
+  //~{
+    //~for (Jj=0;Jj<leftColSize*rightColSize;Jj++)
+    //~{
+      //~val = left(Ii/rightRowSize,Jj/rightColSize);
+      //~val = val * right(Ii%rightRowSize,Jj%rightColSize);
+      //~if (val!=0) { result(Ii,Jj,val); }
+    //~}
+  //~}
+
+  // iterate over only nnz entries
+  Spmat::const_row_iter IiL,IiR;
+  Spmat::const_col_iter JjL,JjR;
+  double valL=0.0,valR=0.0,val=0.0;
+  size_t rowL,colL,rowR,colR,row,col;
+  for(IiL=left._mat.begin(); IiL!=left._mat.end(); IiL++) // loop over all values in left
+  {
+    for( JjL=(IiL->second).begin(); JjL!=(IiL->second).end(); JjL++)
+    {
+      rowL = IiL->first;
+      colL = JjL->first;
+      valL = JjL->second;
+      if (valL==0) {break;}
+
+      // loop over all values in right
+      for(IiR=right._mat.begin(); IiR!=right._mat.end(); IiR++)
+      {
+        for( JjR=(IiR->second).begin(); JjR!=(IiR->second).end(); JjR++)
+        {
+          rowR = IiR->first;
+          colR = JjR->first;
+          valR = JjR->second;
+
+          val = valL*valR;
+          row = rowL*rightRowSize + rowR;
+          col = colL*rightColSize + colR;
+          if (val!=0) { result(row,col,val); }
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+
+
+int spmatTests()
+{
+  PetscErrorCode ierr = 0;
+
+  Spmat mat(3,3);
+  mat(0,0,1); // assign 1 to the (0,0) entry
+  mat(1,2,3);
+
+  mat.printPetsc();
+
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\nTesting Copy of mat:\n");CHKERRQ(ierr);
+  Spmat copied(mat);
+  copied = mat;
+  copied.printPetsc();
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\nTesting transpose of mat:\n");CHKERRQ(ierr);
+  mat.transpose();
+  mat.printPetsc();
+  mat.transpose();
+  //~mat.printPetsc();
+
+
+  double val = mat(0,0);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\n(0,0): val = %f\n",val);CHKERRQ(ierr);
+  val = mat(1,1);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\n(1,1): val = %f\n",val);CHKERRQ(ierr);
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Original values for mat:\n");CHKERRQ(ierr);
+  mat.print();
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Result of scaling mat by 3.0:\n");CHKERRQ(ierr);
+  mat.scale(3.0);
+  mat.printPetsc();
+
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Result of mat.spy():\n");CHKERRQ(ierr);
+  mat.eye();
+  mat.printPetsc();
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\nBuilding new mat:\n");CHKERRQ(ierr);
+  size_t Ii,Jj;
+  val = 1.0;
+  for(Ii=0;Ii<3;Ii++)
+  {
+    for(Jj=0;Jj<3;Jj++)
+    {
+      mat(Ii,Jj,val);
+      val++;
+    }
+  }
+  mat.printPetsc();
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\nkron(mat,eye(1,1)):\n");CHKERRQ(ierr);
+  Spmat I(1,1);
+  I.eye();
+  Spmat result = kron(mat,I);
+  result.printPetsc();
+
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\nkron(mat,eye(2,2)):\n");CHKERRQ(ierr);
+  Spmat I2(2,2);
+  I2.eye();
+  Spmat result2 = kron(mat,I2);
+  result2.printPetsc();
+
+
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"\nkron(eye(2,2),mat):\n");CHKERRQ(ierr);
+  Spmat result3 = kron(I2,mat);
+  result3.printPetsc();
+
+  Mat petscMat;
+  ierr = MatCreate(PETSC_COMM_WORLD,&petscMat);CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject) petscMat, "petscMat");CHKERRQ(ierr);
+  ierr = MatSetSizes(petscMat,PETSC_DECIDE,PETSC_DECIDE,6,6);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(petscMat);CHKERRQ(ierr);
+  ierr = MatMPIAIJSetPreallocation(petscMat,6,NULL,6,NULL);CHKERRQ(ierr);
+  ierr = MatSeqAIJSetPreallocation(petscMat,6,NULL);CHKERRQ(ierr);
+  ierr = MatSetUp(petscMat);CHKERRQ(ierr);
+  result3.convert(petscMat);
+  ierr = MatView(petscMat,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+
+  return ierr;
+}
---
+++ spmat.hpp
@@ -0,0 +1,80 @@
+#ifndef SPMAT_H_INCLUDED
+#define SPMAT_H_INCLUDED
+
+#include <cstdlib>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <assert.h>
+#include <petscts.h>
+
+/* Small class for sparse matrices, supporting a very limited set of
+ * operations.
+ * Based on code from http://www.cplusplus.com/forum/general/8352/
+ */
+
+class Spmat
+{
+public:
+  typedef std::map<size_t, std::map<size_t, double> > mat_t;
+  typedef typename mat_t::iterator row_iter;
+  typedef typename mat_t::const_iterator const_row_iter;
+  typedef std::map<size_t, double> col_t;
+  typedef typename col_t::iterator col_iter;
+  typedef typename col_t::const_iterator const_col_iter;
+
+  Spmat(size_t rowSize, size_t colSize); // constructor
+  Spmat(const Spmat &that); // copy constructor
+  void eye(); // initialize matrix as identity matrix
+
+  // returns the size of the matrix along the specified direction
+  // (1 = row, 2 = col, as in matlab)
+  size_t size(const int dim) const;
+  void scale(double val); // multiply every entry in the matrix by val
+  void transpose();
+  void print() const;
+  void printPetsc() const;
+
+  // convert to PETSc style matrix
+  void convert(Mat& petscMat) const;
+
+  friend Spmat kron(const Spmat& left,const Spmat& right);
+
+
+  // inline functions are defined in the header file
+
+  // insert a value into the matrix
+  inline void operator()(size_t row, size_t col,double val)
+  {
+    assert(row<_rowSize && col<_colSize);
+    _mat[row][col] = val;
+  };
+
+  // return value at (row,col) from the matrix
+  inline double operator()(size_t row, size_t col) const
+  {
+    assert(row<_rowSize && col<_colSize);
+
+    const_row_iter it = _mat.find(row);
+    if ( it == _mat.end() ) { return 0.0; }
+    const_col_iter temp = it->second.find(col);
+    if ( temp == it->second.end() ) { return 0.0; }
+    return it->second.find(col)->second;
+    //~return _mat[row][col]; // this creates new entry if it doesn't exist
+  };
+
+  //~private:
+  protected:
+    mat_t _mat;
+    size_t _rowSize;
+    size_t _colSize;
+
+    // disable default assignment operator
+    //~Spmat& operator=(const Spmat &rhs);
+
+};
+
+//~Mat& kron(const Spmat& left,const Spmat& right);
+int spmatTests();
+
+#endif
