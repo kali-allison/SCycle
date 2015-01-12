717c314bceb0d4836b3f5d4c66d0f4b972fac11b
Kali Allison <kallison@stanford.edu>
2015-01-12 02:33:09 -0800

    fixed all problems: changed vp in basin.in and test.in to 1e-9 (from 1e-4), fixed initial gR, calling lith not asthenosphere

--- runcode.sh
+++ runcode.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#PBS -N eqCycle
-#PBS -l nodes=4:ppn=16
+#PBS -N 4_13_muIn36_LU
+#PBS -l nodes=1:ppn=16
 #PBS -q tgp
 #PBS -V
 #PBS -m n
 #PBS -k oe
-#PBS -e data/err.txt
-#PBS -o data/out.txt
+#PBS -e data/4_13_muIn36.err
+#PBS -o data/4_13_muIn36.out
 #
 
 EXEC_DIR=/data/dunham/kallison/eqCycle
 INIT_DIR=$EXEC_DIR
 cd $PBS_O_WORKDIR
 
-mpirun $EXEC_DIR/main $INIT_DIR/init_mu_9.in
+mpirun $EXEC_DIR/main $INIT_DIR/basin.in
