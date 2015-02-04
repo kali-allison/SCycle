8d76ceef23f9afa4c9c7014b6df7e9e44db26bfd
Kali Allison <kallison@stanford.edu>
2015-02-03 22:14:39 -0800

    fixed implementation of rootfinding algorithms to using an interface

---
+++ rootFinderContext.hpp
@@ -0,0 +1,23 @@
+#ifndef ROOTFINDERCONTEXT_H_INCLUDED
+#define ROOTFINDERCONTEXT_H_INCLUDED
+
+#include <petscksp.h>
+#include <vector>
+
+/*
+ * This abstract class defines an interface for RootFinder. Classes
+ * that will use RootFinder routines must implement the virtual function.
+ */
+
+class RootFinder;
+
+class RootFinderContext
+{
+  public:
+
+    virtual PetscErrorCode getResid(const PetscInt ind,const PetscScalar vel,PetscScalar *out) = 0;
+};
+
+#include "rootfinder.hpp"
+
+#endif
