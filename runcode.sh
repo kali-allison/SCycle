d39e4bffd18b474a8dd9fcb700708115565700ba
Kali Allison <kallison@stanford.edu>
2015-03-08 18:27:38 -0700

    set up realistic gradient normal stress

--- runcode.sh
+++ runcode.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#PBS -N full_basin
-#PBS -l nodes=1:ppn=16
+#PBS -N gradNormal
+#PBS -l nodes=4:ppn=16
 #PBS -q tgp
 #PBS -V
 #PBS -m n
 #PBS -k oe
-#PBS -e data/full_basin.err
-#PBS -o data/full_basin.out
+#PBS -e data/gradNormal.err
+#PBS -o data/gradNormal.out
 #
 
 EXEC_DIR=/data/dunham/kallison/eqCycle
 INIT_DIR=$EXEC_DIR
 cd $PBS_O_WORKDIR
 
-mpirun $EXEC_DIR/main $INIT_DIR/test.in
+mpirun $EXEC_DIR/main $INIT_DIR/basin.in
