3c0fd6c8c21f27cb45eed3cedddc933fc3ec86c5
Kali Allison <kallison@stanford.edu>
2015-11-28 18:39:37 -0800

    keeping changes to test.in

--- runcode.sh
+++ runcode.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#PBS -N mvfc_v1e20_rate_bc
+#PBS -N lefc_16e-3
 #PBS -l nodes=1:ppn=16
 #PBS -q tgp
 #PBS -V
 #PBS -m n
 #PBS -k oe
-#PBS -e /data/dunham/kallison/newEqCycle/data/mvfc_v1e20_rate_bc.err
-#PBS -o /data/dunham/kallison/newEqCycle/data/mvfc_v1e20__rate_bc.out
+#PBS -e /data/dunham/kallison/newEqCycle/data/lec_16e-3.err
+#PBS -o /data/dunham/kallison/newEqCycle/data/lec_16e-3.out
 #
 
 EXEC_DIR=/data/dunham/kallison/newEqCycle
 INIT_DIR=$EXEC_DIR
 cd $PBS_O_WORKDIR
 
-mpirun $EXEC_DIR/main $INIT_DIR/test.in
+mpirun $EXEC_DIR/main_linearElastic $INIT_DIR/test.in
