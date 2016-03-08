8e0e7ef79565b289d78f9a091730e892ba5f01e5
Kali Allison <kallison@stanford.edu>
2016-03-07 21:53:11 -0800

    preparing to time DMDA mat vs stencil-based derivative

--- mainLinearElastic.cpp
+++ mainLinearElastic.cpp
@@ -167,47 +167,58 @@ int runTests2D()
   PetscInt starts[2] = {zS,yS};
   MatSetStencil(mat,dim,dims,starts,1); // be sure to set with DMDAGetGhostCorners!!
 
-  // create 1st derivative matrix (2nd order accuracy)
-  PetscMPIInt localRank;
-  MPI_Comm_rank(PETSC_COMM_WORLD,&localRank);
+  // create Dz matrix (2nd order accuracy)
   DMDAGetCorners(da, &zS, &yS, 0, &zn, &yn, 0);
-  MatStencil row,col;
-  PetscScalar v = 0.0;
+  MatStencil row,col[2];
+  PetscScalar v[2] = {0.0,0.0};
+  //~for (PetscInt yI = yS; yI < yS + yn; yI++) {
+    //~for (PetscInt zI = zS; zI < zS + zn; zI++) {
+      //~row.i = zI; row.j = yI;
+
+      //~// interior
+      //~if (zI > 0 && zI < Nz-1) {
+        //~col[0].i = zI-1; col[0].j = yI; v[0] = -0.5/dz;
+        //~col[1].i = zI+1; col[1].j = yI; v[1] = 0.5/dz;
+        //~MatSetValuesStencil(mat,1,&row,2,col,v,INSERT_VALUES);
+      //~}
+      //~else if (zI == 0) { // boundary
+        //~col[0].i = zI; col[0].j = yI; v[0] = -1.0/dz;
+        //~col[1].i = zI+1; col[1].j = yI; v[1] = 1.0/dz;
+        //~MatSetValuesStencil(mat,1,&row,2,col,v,INSERT_VALUES);
+      //~}
+      //~else if (zI == Nz-1) { // boundary
+        //~col[0].i = zI-1; col[0].j = yI; v[0] = -1.0/dz;
+        //~col[1].i = zI; col[1].j = yI; v[1] = 1.0/dz;
+        //~MatSetValuesStencil(mat,1,&row,2,col,v,INSERT_VALUES);
+      //~}
+    //~}
+  //~}
+  //~MatAssemblyBegin(mat,MAT_FINAL_ASSEMBLY);
+  //~MatAssemblyEnd(mat,MAT_FINAL_ASSEMBLY);
+  //~MatView(mat,PETSC_VIEWER_STDOUT_WORLD);
+
+  // create Dy matrix (2nd order accuracy)
+  DMDAGetCorners(da, &zS, &yS, 0, &zn, &yn, 0);
   for (PetscInt yI = yS; yI < yS + yn; yI++) {
     for (PetscInt zI = zS; zI < zS + zn; zI++) {
-      PetscPrintf(PETSC_COMM_WORLD,"(yI,zI) = (%i,%i)\n",yI,zI);
       row.i = zI; row.j = yI;
 
-
-      // interior of Dz matrix (slow method):
-      if (zI > 0 && zI < Nz-1) {
-      col.i = zI-1; col.j = yI;
-      v = -0.5;
-      MatSetValuesStencil(mat,1,&row,1,&col,&v,INSERT_VALUES);
-
-      col.i = zI+1; col.j = yI;
-      v = 0.5;
-      MatSetValuesStencil(mat,1,&row,1,&col,&v,INSERT_VALUES);
-    }
-
-
-
-    //~row.i = zI;
-    //~if (zI > 0 && zI < N-1) {
-      //~col[0].i = zI-1; v[0] = -0.5/dz;
-      //~col[1].i = zI+1; v[1] = 0.5/dz;
-      //~MatSetValuesStencil(mat,1,&row,2,col,v,INSERT_VALUES);
-    //~}
-    //~else if (zI == 0) {
-      //~col[0].i = zI; v[0] = -1.0/dz;
-      //~col[1].i = zI+1; v[1] = 1.0/dz;
-      //~MatSetValuesStencil(mat,1,&row,2,col,v,INSERT_VALUES);
-    //~}
-    //~else if (zI == N-1) {
-      //~PetscPrintf(PETSC_COMM_WORLD,"here!!\n");
-      //~col[0].i = zI-1; v[0] = -1.0/dz;
-      //~col[1].i = zI; v[1] = 1.0/dz;
-      //~MatSetValuesStencil(mat,1,&row,2,col,v,INSERT_VALUES);
+      // interior
+      if (yI > 0 && yI < Ny-1) {
+        col[0].i = zI; col[0].j = yI-1; v[0] = -0.5/dy;
+        col[1].i = zI; col[1].j = yI+1; v[1] = 0.5/dy;
+        MatSetValuesStencil(mat,1,&row,2,col,v,INSERT_VALUES);
+      }
+      else if (yI == 0) { // boundary
+        col[0].i = zI; col[0].j = yI; v[0] = -1.0/dy;
+        col[1].i = zI; col[1].j = yI+1; v[1] = 1.0/dy;
+        MatSetValuesStencil(mat,1,&row,2,col,v,INSERT_VALUES);
+      }
+      else if (yI == Ny-1) { // boundary
+        col[0].i = zI; col[0].j = yI-1; v[0] = -1.0/dy;
+        col[1].i = zI; col[1].j = yI; v[1] = 1.0/dy;
+        MatSetValuesStencil(mat,1,&row,2,col,v,INSERT_VALUES);
+      }
     }
   }
   MatAssemblyBegin(mat,MAT_FINAL_ASSEMBLY);
