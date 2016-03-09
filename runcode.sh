e270f25911b3e783662cb62946888d581c726fac
Kali Allison <kallison@stanford.edu>
2016-03-08 23:17:42 -0800

    trying to figure out why effective viscosity is so low

--- runcode.sh
+++ runcode.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#PBS -N m2D_401
+#PBS -N m2D_v5e12
 #PBS -l nodes=1:ppn=16
 #PBS -q tgp
 #PBS -V
 #PBS -m n
 #PBS -k oe
-#PBS -e /data/dunham/kallison/newEqCycle/data/m2D_401.err
-#PBS -o /data/dunham/kallison/newEqCycle/data/m2D_401.out
+#PBS -e /data/dunham/kallison/newEqCycle/data/m2D_v5e12.err
+#PBS -o /data/dunham/kallison/newEqCycle/data/m2D_v5e12.out
 #
 
 EXEC_DIR=/data/dunham/kallison/newEqCycle
