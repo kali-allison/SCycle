a7d90a860cee15d3eae5dff7b285261c94daea0e
Kali <kallison@stanford.com>
2016-05-10 19:21:22 -0700

    updated nehrp set up

--- runMazama.sh
+++ runMazama.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#PBS -N m2D_grad
+#PBS -N m2D_nehrp
 #PBS -l nodes=1:ppn=24
 #PBS -q tgp
 #PBS -V
 #PBS -m n
 #PBS -k oe
-#PBS -e /data/dunham/kallison/eqcycle/data/max/m2D_grad.err
-#PBS -o /data/dunham/kallison/eqcycle/data/max/m2D_grad.out
+#PBS -e /data/dunham/kallison/eqcycle/data/nehrp/nehrp.err
+#PBS -o /data/dunham/kallison/eqcycle/data/nehrp/nehrp.out
 #
 
 EXEC_DIR=/data/dunham/kallison/eqcycle
 INIT_DIR=$EXEC_DIR
 cd $PBS_O_WORKDIR
 
-mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/linEl2D.in
+mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/nehrp.in
+#~mpirun $EXEC_DIR/main $INIT_DIR/powerLaw2D.in
