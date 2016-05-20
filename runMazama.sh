4bf5241129bb0d8965e568be5f7e94aa4cdd6c20
Kali <kallison@stanford.com>
2016-05-20 14:34:15 -0700

    removed fault heating from heat equation

--- runMazama.sh
+++ runMazama.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#PBS -N l2D_wT
+#PBS -N m2D_nT
 #PBS -l nodes=1:ppn=24
 #PBS -q default
 #PBS -V
 #PBS -m n
 #PBS -k oe
-#PBS -e /data/dunham/kallison/eqcycle/data/nehrp/l2D_wT.err
-#PBS -o /data/dunham/kallison/eqcycle/data/nehrp/l2D_wTout
+#PBS -e /data/dunham/kallison/eqcycle/data/m2D_nT.err
+#PBS -o /data/dunham/kallison/eqcycle/data/m2D_nTout
 #
 
 EXEC_DIR=/data/dunham/kallison/eqcycle
 INIT_DIR=$EXEC_DIR
 cd $PBS_O_WORKDIR
 
-#~ mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/linEl2D.in
-#~ mpirun $EXEC_DIR/main $INIT_DIR/linEl2D.in
-mpirun $EXEC_DIR/mainLinearElastic $INIT_DIR/m2D.in
+mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/m2D.in
+#~ mpirun $EXEC_DIR/main $INIT_DIR/m2D.in
+#~ mpirun $EXEC_DIR/mainLinearElastic $INIT_DIR/m2D.in
