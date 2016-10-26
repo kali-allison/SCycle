028a89ddeeaca2aec17a7ee4a4940931996a9d33
Kali <kallison@stanford.com>
2016-10-26 16:36:00 -0700

    added Blanpied friction data to input files, create spin up info for it for linear elastic simulations

--- runMazama.sh
+++ runMazama.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#PBS -N tauSS_Nz101
+#PBS -N blanpied_Nz701
 #PBS -l nodes=1:ppn=24
 #PBS -q tgp
 #PBS -V
 #PBS -m n
 #PBS -k oe
-#PBS -e /data/dunham/kallison/eqcycle/data/tauSS_Nz101.err
-#PBS -o /data/dunham/kallison/eqcycle/data/tauSS_Nz101.out
+#PBS -e /data/dunham/kallison/eqcycle/spinUpTests/testSpinUp2_blanpied_Dc8e-3_Nz701.err
+#PBS -o /data/dunham/kallison/eqcycle/spinUpTests/testSpinUp2_blanpied_Dc8e-3_Nz701.out
 
 
 EXEC_DIR=/data/dunham/kallison/eqcycle
@@ -15,10 +15,10 @@ INIT_DIR=/data/dunham/kallison/eqcycle
 #~ INIT_DIR=/scratch/kallison
 cd $PBS_O_WORKDIR
 
-#~ mpirun $EXEC_DIR/mainLinearElastic $INIT_DIR/m2D.in
+mpirun $EXEC_DIR/mainLinearElastic $INIT_DIR/m2D.in
 #~ mpirun $EXEC_DIR/mainLinearElastic $INIT_DIR/h2D.in
 #~ mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/m2D.in
-mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/m2D_spinUp.in
+#~ mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/m2D_spinUp.in
 #~ mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/mms.in
 #~ mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/h2D.in
 #~ mpirun $EXEC_DIR/main $INIT_DIR/m2D.in
