8b82663821ee880db5738bea5e6912bf063c4f2a
Kali <kallison@stanford.com>
2016-10-31 17:50:36 -0700

    fixed parameters in jelly sandwich input file

--- runMazama.sh
+++ runMazama.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#PBS -N cremeBrulee_blansVW_spinUp1_Ny151_Nz151
+#PBS -N jellySandwich_blansVW_spinUp1_Ny151_Nz251
 #PBS -l nodes=1:ppn=24
 #PBS -q tgp
 #PBS -V
 #PBS -m n
 #PBS -k oe
-#PBS -e /data/dunham/kallison/eqcycle/cremeBrulee_blansVW_spinUp1_Ny151_Nz151.err
-#PBS -o /data/dunham/kallison/eqcycle/cremeBrulee_blansVW_spinUp1_Ny151_Nz151.out
+#PBS -e /data/dunham/kallison/eqcycle/jellySandwich_blansVW_spinUp1_Ny151_Nz251.err
+#PBS -o /data/dunham/kallison/eqcycle/jellySandwich_blansVW_spinUp1_Ny151_Nz251.out
 
 
 EXEC_DIR=/data/dunham/kallison/eqcycle
@@ -18,7 +18,8 @@ cd $PBS_O_WORKDIR
 #~ mpirun $EXEC_DIR/mainLinearElastic $INIT_DIR/m2D.in
 #~ mpirun $EXEC_DIR/mainLinearElastic $INIT_DIR/h2D.in
 #~ mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/max.in
-mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/max_cremeBrulee_spinUp1.in
+#~ mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/max_cremeBrulee_spinUp1.in
+mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/max_jellySandwich_spinUp1.in
 #~ mpirun $EXEC_DIR/mainMaxwell $INIT_DIR/max_spinUp.in
 #~ mpirun $EXEC_DIR/main $INIT_DIR/max.in
 #~ mpirun $EXEC_DIR/main $INIT_DIR/max_spinUp.in
