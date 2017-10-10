c190880ed3e0f2c9711ba9385393ef7f32f54552
kallison <kallison@stanford.edu>
2017-10-10 10:27:52 -0700

    added simple 1 block, no fault example problems for dynamic wave equation

---
+++ matlab/dynamicEq/Dzz_mu.m
@@ -0,0 +1,27 @@
+function u_zz = Dzz_mu(u,G,h,order,bcType)
+
+if nargin < 5
+  bcType = 'not periodic';
+end
+
+u_zz = 0*u;
+[m,n] = size(u);
+
+if(order == 2)
+  if strcmp(bcType,'periodic')
+    u_yy(:,1) = u(:,n-1) - 2*u(:,1) + u(:,2);
+    u_yy(:,end) =u(:,n-1) - 2*u(:,end) + u(:,2);
+  end
+  for ind = 2:n-1
+     u_zz(:,ind) = u(:,ind+1) - 2*u(:,ind) + u(:,ind-1);
+     
+    u_zz(:,ind) = 0.5*(G(:,ind)+G(:,ind-1)).*u(:,ind-1) ...
+      - 0.5*(G(:,ind+1)+2*G(:,ind)+G(:,ind-1)).*u(:,ind) ...
+      + 0.5*(G(:,ind)+G(:,ind+1)).*u(:,ind+1);
+  end
+elseif(order == 4)
+  'Error: 4th order accuracy not yet supported!!'
+end
+
+u_zz = (1/h^2)*u_zz;
+end
