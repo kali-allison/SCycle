fbbea1b56f4af1418c84beefba5bdf311c0f7659
IFDYS <wayne.weiqiang@gmail.com>
2017-11-10 06:19:00 +0000

    add backward eular

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int runMMSTests(const char * inputFile)
   PetscPrintf(PETSC_COMM_WORLD,"%-3s %-2s %-10s %-10s %-22s %-10s %-22s %-10s %-22s\n",
              "ord","Ny","dy","errL2u","log2(errL2u)","errL2gxy","log2(errL2gxy)",
              "errL2gxz","log2(errL2gxz)");
-  for(PetscInt Ny=11;Ny<82;Ny=(Ny-1)*2+1)
+  for(PetscInt Ny=11;Ny<12;Ny=(Ny-1)*2+1)
   //~ for(PetscInt Ny=11;Ny<12;Ny=(Ny-1)*2+1)
   {
     Domain d(inputFile,Ny,Ny);
