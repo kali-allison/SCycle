18c408d034e4663e0cccb4d5ea2c072e26144888
Maxime Rivet <maxime.rivet92@gmail.com>
2018-05-15 23:08:51 -0700

    Corrected problem that killed the output

--- strikeSlip_linearElastic_switch.cpp
+++ strikeSlip_linearElastic_switch.cpp
@@ -517,6 +517,8 @@ PetscErrorCode StrikeSlip_LinearElastic_switch::reset_for_qd(){
   // Force writing output
   if(_stepCount % _stride1D > 0){
     PetscInt stride1d, stride2d;
+    stride1d = _stride1D;
+    stride2d = _stride2D;
     _stride1D = 1;
     _stride2D = 1;
     timeMonitor(_currTime, _stepCount, _varEx, _varEx, _stride1D);
