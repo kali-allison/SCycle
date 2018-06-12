06183adff55358720aee34e02563021882021e2b
Kali <kallison@stanford.com>
2018-06-12 13:34:39 -0700

    added license and manual

--- runMazama.sh
+++ runMazama.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#PBS -N n1_Nz3329_AMG
+#PBS -N wvsh_zlab60_l0.37_w10
 #PBS -l nodes=1:ppn=24
-#PBS -q default
+#PBS -q tgp
 #PBS -V
 #PBS -m n
 #PBS -k oe
-#PBS -e /data/dunham/kallison/eqcycle/outTime/n1_Nz3329_AMG.err
-#PBS -o /data/dunham/kallison/eqcycle/outTime/n1_Nz3329_AMG.out
+#PBS -e /data/dunham/kallison/scycle/outFiles/wvsh_zlab60_l0.37_w10.err
+#PBS -o /data/dunham/kallison/scycle/outFiles/wvsh_zlab60_l0.37_w10.out
 
-EXEC_DIR=/data/dunham/kallison/eqcycle
-INIT_DIR=/data/dunham/kallison/eqcycle/in
+EXEC_DIR=/data/dunham/kallison/scycle
+INIT_DIR=/data/dunham/kallison/scycle/tempIn
 cd $PBS_O_WORKDIR
 
 #~ mpirun $EXEC_DIR/main $INIT_DIR/he_ssits.in
 #~ mpirun $EXEC_DIR/main $INIT_DIR/he_eqs.in
 #~ mpirun $EXEC_DIR/main $INIT_DIR/linEl.in
-mpirun $EXEC_DIR/main $INIT_DIR/n1_Nz3329_AMG.in
+mpirun $EXEC_DIR/main $INIT_DIR/SS_wvsh_zlab60_l0.37_w10.in
 #~ mpirun $EXEC_DIR/main $INIT_DIR/heCont.in
 
