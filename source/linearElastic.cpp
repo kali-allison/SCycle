48f02455bbee17de263f11990cecad11e48c94aa
Kali Allison <kaliallison4@gmail.com>
2021-06-27 19:24:03 -0400

    changed PCBJacobi sub KSP to initial guess nonzero

--- source/linearElastic.cpp
+++ source/linearElastic.cpp
@@ -305,9 +305,12 @@ PetscErrorCode LinearElastic::setupKSP(KSP& ksp,PC& pc,Mat& A,std::string& linSo
 
     // Loop over the local blocks, setting various KSP options for each block.
     for (ii=0; ii<nlocal; ii++) {
-      ierr = KSPGetPC(subksp[ii],&subpc);CHKERRQ(ierr);
-      ierr = PCSetType(subpc,PCILU);CHKERRQ(ierr);
+      ierr = KSPGetPC(subksp[ii],&subpc);                               CHKERRQ(ierr);
+      ierr = PCSetType(subpc,PCILU);                                    CHKERRQ(ierr);
+      ierr = KSPSetInitialGuessNonzero(subksp[ii], PETSC_TRUE);                CHKERRQ(ierr);
+      ierr = KSPSetReusePreconditioner(subksp[ii],PETSC_TRUE);                 CHKERRQ(ierr);
     }
+
 
   }
 
