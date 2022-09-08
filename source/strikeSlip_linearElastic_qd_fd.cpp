f53ac0125e785573be2925efd7d977229004574c
Kali Allison <kaliallison4@gmail.com>
2022-09-08 15:12:32 -0700

    removed extra print

--- source/strikeSlip_linearElastic_qd_fd.cpp
+++ source/strikeSlip_linearElastic_qd_fd.cpp
@@ -167,11 +167,7 @@ PetscErrorCode StrikeSlip_LinearElastic_qd_fd::loadSettings(const char *file)
     pos = rhs.find(" ");
     rhs = rhs.substr(0,pos);
 
-    if (var.compare("thermalCoupling")==0) {
-      _thermalCoupling = rhs.c_str();
-      PetscPrintf(PETSC_COMM_WORLD,"thermalCoupling = %s\n",_thermalCoupling.c_str());
-      assert(0);
-      }
+    if (var.compare("thermalCoupling")==0) { _thermalCoupling = rhs.c_str(); }
     else if (var.compare("hydraulicCoupling")==0) { _hydraulicCoupling = rhs.c_str(); }
     else if (var.compare("stateLaw")==0) { _stateLaw = rhs.c_str(); }
     else if (var.compare("guessSteadyStateICs")==0) { _guessSteadyStateICs = atoi(rhs.c_str() ); }
@@ -308,10 +304,10 @@ PetscErrorCode StrikeSlip_LinearElastic_qd_fd::checkInput()
   if (_limit_stride_fd == -1){
     _limit_stride_fd = _limit_fd / 10.0;
   }
-  if (_thermalCoupling != "no" && (_timeIntegrator != "RK32_WBE" && _timeIntegrator != "RK43_WBE")) {
-    PetscPrintf(PETSC_COMM_WORLD,"thermal coupling = %s\n",_thermalCoupling.c_str());
+  //~ if (_thermalCoupling != "no" && (_timeIntegrator != "RK32_WBE" && _timeIntegrator != "RK43_WBE")) {
+    //~ PetscPrintf(PETSC_COMM_WORLD,"thermal coupling = %s\n",_thermalCoupling.c_str());
     //~ assert(0);
-  }
+  //~ }
 
   #if VERBOSE > 1
     ierr = PetscPrintf(PETSC_COMM_WORLD,"Ending %s in %s\n",funcName.c_str(),FILENAME);
